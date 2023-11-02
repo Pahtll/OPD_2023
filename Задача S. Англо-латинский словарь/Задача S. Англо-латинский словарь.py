19391414e0b2e75da6993477d80615a28f3bca5a
Vadim Denisovich <149379180+VadimDenisovich@users.noreply.github.com>
2023-11-02 17:11:48 +1000

    Create Задача S. Англо-латинский словарь.py
    Не самая оптимизированная задача, нужны правки сеньора

---
+++ Задача S. Англо-латинский словарь/Задача S. Англо-латинский словарь.py
@@ -0,0 +1,21 @@
+n, words = int(input()), {}
+for _ in range(n):
+    el = input().replace("-",'').replace(',','').split()
+    for i in range(1, len(el)):
+        if el[i] not in words:
+            words[el[i]] = [el[0]]
+        else:
+            words[el[i]] += [el[0]]
+words, res, tres = sorted([(ell, words[ell]) for ell in words]), '', ''
+for el in words:
+    tres += f'{el[0]} - '
+    if len(el[1]) > 1:
+        for n in el[1]:
+            tres += f'{n}, '
+        tres = tres[:-2]
+    else:
+        tres += f'{el[1][0]}'
+    tres += '\n'
+    res += tres
+    tres = ''
+print(len(words), res, sep='\n')
