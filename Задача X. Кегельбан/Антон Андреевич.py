ca51e2e7df59320937e5f05aa219f5bd03af0c31
4444urka <psinka2006@gmail.com>
2023-11-03 15:20:19 +1000

    Create Антон Андреевич.py

---
+++ Задача X. Кегельбан/Антон Андреевич.py
@@ -0,0 +1,11 @@
+#Считываем N,K как целые числа
+N,K = map(int, input().split())
+#Создаём список длиной N, состоящий из кеглей
+ryad = N*['I']
+for _ in range(K):
+    #Считываем с какие кегли сбили (от k1 до k2) и заменяем их на точки
+    k1, k2 = map(int, input().split())
+    for keglya in range(k1-1, k2):
+        ryad[keglya] = '.'
+#Переводим список в строку и выводим
+print(''.join(ryad))
