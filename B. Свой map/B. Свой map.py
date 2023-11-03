5c0bf92ad6c530e2c5dc1ebf94bacd7af6f39c1f
Vadim Denisovich <149379180+VadimDenisovich@users.noreply.github.com>
2023-11-03 19:53:59 +1000

    Create B. Свой map - Вадим Денисович.py

---
+++ B. Свой map/B. Свой map.py
@@ -0,0 +1,11 @@
+def map(func, values):
+    new_lst = []
+    for el in values:
+        new_lst.append(func(el))
+    return new_lst
+
+# Можете ввести свою функцию и значения
+operation = lambda x: x + 5
+values = [1, 3, 1, 5, 7]
+
+print(map(operation, values))
