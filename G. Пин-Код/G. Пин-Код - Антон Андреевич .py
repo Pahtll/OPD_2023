120bc83373ecfd0bc1b0d0f4fd0417d2d49f16bf
4444urka <psinka2006@gmail.com>
2023-11-08 12:04:11 +1000

    Update G. Пин-Код - Антон Андреевич .py

--- G. Пин-Код/G. Пин-Код - Антон Андреевич .py
+++ G. Пин-Код/G. Пин-Код - Антон Андреевич .py
@@ -6,7 +6,9 @@ def isPrime(a):
         d+=1
     return d==a
 def check_pin(pinCode):
+    #Делим строку на 3 числа
     ListPins = pinCode.split('-')
+    #Первое число проверяем на простоту, второе число проверяем на палиндром, проверяем являетя ли третье число степенью двойки при помощи логарифма
     if isPrime(int(ListPins[0])) and  ListPins[1] == ListPins[1][::-1] and log(int(ListPins[2]),2)  %  1 == 0:
         return 'Корректен'
     else:
