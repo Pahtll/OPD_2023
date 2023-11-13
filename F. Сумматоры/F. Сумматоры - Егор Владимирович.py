55d8fe05f967fa31a6d6be9fcb0debbe96c409cd
Pahtll <astahov.egor008@gmail.com>
2023-11-13 11:38:25 +1000

    Навёл красоту F. Сумматоры

--- F. Сумматоры/F. Сумматоры - Егор Владимирович.py
+++ F. Сумматоры/F. Сумматоры - Егор Владимирович.py
@@ -2,27 +2,32 @@ class Summator:
     def __init__(self, n: int) -> None:
         self.sum = 0
         self.n = n
+    #Трансформ ничего не меняет, изменять значения он будет в дочерних классах
     def Transform(self, n: int) -> int:
         return n
+    #Сумм, очевидно суммирует все элементы от 0, до N (включительно)
     def Sum(self) -> int:
         for num in range(0, self.n + 1):
             self.sum += self.Transform(num)
         return self.sum
     
 class SquareSummator(Summator):
+    #Проводим инициализацию по иницаиализации класса родителя для дочерних классов
     def __init__(self, n: int) -> None:
-        self.sum = 0
-        self.n = n
+        super().__init__(n)
+        
     def Transform(self, n: int) -> int:
         return super().Transform(n**2)    
 
 class CubeSummator(Summator):
     def __init__(self, n: int) -> None:
-        self.sum = 0
-        self.n = n
+        super().__init__(n)
+        
     def Transform(self, n: int) -> int:
         return super().Transform(n**3)
-    
+
+
+#Тесты    
 cube = CubeSummator(15)
 square = SquareSummator(15)
 
